561e36c746dc35754b51456fa27cdd4380dc438b
bineethkumar <bineeth7@gmail.com>
2021-07-22 10:56:00 +0530

    add mongoose models

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
 //Database
-const database = require("./database");
+const database = require("./database/database");
 //Initialization
 const booky = express();
 //configuration
